8f155a884ae10158774ca650cab9299de9dbaa84
Neale Upstone <neale@nealeupstone.com>
2019-04-11 09:19:05 +0100

    Cleanup most IDEA warnings in index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -2,21 +2,21 @@
 
 // having weak reference to styles prevents garbage collection
 // and "losing" styles when the next test starts
-const stylesCache = new Map()
+const stylesCache = new Map();
 
 const setXMLHttpRequest = w => {
   // by grabbing the XMLHttpRequest from app's iframe
   // and putting it here - in the test iframe
   // we suddenly get spying and stubbing 😁
   // @ts-ignore
-  window.XMLHttpRequest = w.XMLHttpRequest
+  window.XMLHttpRequest = w.XMLHttpRequest;
   return w
-}
+};
 
 const setAlert = w => {
-  window.alert = w.alert
+  window.alert = w.alert;
   return w
-}
+};
 
 /** Initialize an empty document w/ ReactDOM and DOM events.
     @function   cy.injectReactDOM
@@ -26,7 +26,7 @@ Cypress.Commands.add('injectReactDOM', () => {
     // Generate inline script tags for UMD modules
     const scripts = Cypress.modules
       .map(module => `<script>${module.source}</script>`)
-      .join('')
+      .join('');
     // include React and ReactDOM to force DOM to register all DOM event listeners
     // otherwise the component will NOT be able to dispatch any events
     // when it runs the second time
@@ -39,15 +39,15 @@ Cypress.Commands.add('injectReactDOM', () => {
     <body>
       <div id="cypress-jsdom"></div>
       ${scripts}
-    </body>`
+    </body>`;
 
-    const document = cy.state('document')
-    document.write(html)
+    const document = cy.state('document');
+    document.write(html);
     document.close()
   })
-})
+});
 
-Cypress.stylesCache = stylesCache
+Cypress.stylesCache = stylesCache;
 
 /** Caches styles from previously compiled components for reuse
     @function   cy.copyComponentStyles
@@ -57,19 +57,19 @@ Cypress.Commands.add('copyComponentStyles', component => {
   // need to find same component when component is recompiled
   // by the JSX preprocessor. Thus have to use something else,
   // like component name
-  const parentDocument = window.parent.document
+  const parentDocument = window.parent.document;
   // @ts-ignore
-  const specDocument = parentDocument.querySelector('iframe.spec-iframe').contentDocument
+  const specDocument = parentDocument.querySelector('iframe.spec-iframe').contentDocument;
   // @ts-ignore
-  const appDocument = parentDocument.querySelector('iframe.aut-iframe').contentDocument
+  const appDocument = parentDocument.querySelector('iframe.aut-iframe').contentDocument;
 
-  const hash = component.type.name
-  let styles = specDocument.querySelectorAll('head style')
+  const hash = component.type.name;
+  let styles = specDocument.querySelectorAll('head style');
   if (styles.length) {
-    cy.log(`injected ${styles.length} style(s)`)
+    cy.log(`injected ${styles.length} style(s)`);
     Cypress.stylesCache.set(hash, styles)
   } else {
-    cy.log('No styles injected for this component, checking cache')
+    cy.log('No styles injected for this component, checking cache');
     if (Cypress.stylesCache.has(hash)) {
       styles = Cypress.stylesCache.get(hash)
     } else {
@@ -79,18 +79,18 @@ Cypress.Commands.add('copyComponentStyles', component => {
   if (!styles) {
     return
   }
-  const head = appDocument.querySelector('head')
+  const head = appDocument.querySelector('head');
   styles.forEach(function (style) {
     head.appendChild(style)
   })
-})
+});
 
 /**
  * Mount a React component in a blank document; register it as an alias
  * To access: use an alias or original component reference
  *  @function   cy.mount
  *  @param      {Object}  jsx - component to mount
- *  @param      {string}  [Component] - alias to use later
+ *  @param      {string}  alias [Component] - alias to use later
  *  @example
  ```
  import Hello from './hello.jsx'
@@ -104,11 +104,11 @@ Cypress.Commands.add('copyComponentStyles', component => {
  cy.get(Hello)
  ```
  **/
-export const mount = (jsx, alias) => {
+export const mount = (jsx, alias?: string) => {
   // Get the display name property via the component constructor
-  const displayname = alias || jsx.type.prototype.constructor.name
+  const displayname = alias || jsx.type.prototype.constructor.name;
 
-  let cmd
+  let cmd;
 
   cy.injectReactDOM()
     .window({ log: false })
@@ -127,21 +127,21 @@ export const mount = (jsx, alias) => {
     .then(setXMLHttpRequest)
     .then(setAlert)
     .then(win => {
-      const { ReactDOM } = win
-      const document = cy.state('document')
+      const { ReactDOM } = win;
+      const document = cy.state('document');
       const component = ReactDOM.render(
         jsx,
         document.getElementById('cypress-jsdom')
-      )
+      );
       cy.wrap(component, { log: false }).as(displayname)
-    })
+    });
   cy.copyComponentStyles(jsx)
     .then(() => {
       cmd.snapshot().end()
     })
-}
+};
 
-Cypress.Commands.add('mount', mount)
+Cypress.Commands.add('mount', mount);
 
 /** Get one or more DOM elements by selector or alias.
     Features extended support for JSX and React.Component
@@ -160,17 +160,17 @@ Cypress.Commands.overwrite('get', (originalFn, selector, options) => {
         selector.$$typeof &&
         selector.$$typeof.toString().startsWith('Symbol(react')
       ) {
-        const displayname = selector.type.prototype.constructor.name
+        const displayname = selector.type.prototype.constructor.name;
         return originalFn(`@${displayname}`, options)
       }
     case 'function':
       // If attempting to use the component name without JSX (testing in .js/.ts files)
-      const displayname = selector.prototype.constructor.name
-      return originalFn(`@${displayname}`, options)
+      const displayname = selector.prototype.constructor.name;
+      return originalFn(`@${displayname}`, options);
     default:
       return originalFn(selector, options)
   }
-})
+});
 
 /*
 Before All
@@ -180,7 +180,7 @@ Before All
   Format: [{name, type, location}, ...]
 */
 before(() => {
-  const settings = Cypress.env('cypress-react-unit-test') || {}
+  const settings = Cypress.env('cypress-react-unit-test') || {};
 
   const moduleNames = [
     {
@@ -193,14 +193,14 @@ before(() => {
       type: 'file',
       location: settings['react-dom'] || 'node_modules/react-dom/umd/react-dom.development.js'
     }
-  ]
+  ];
 
-  Cypress.modules = []
+  Cypress.modules = [];
   cy.log('Initializing UMD module cache').then(() => {
     for (const module of moduleNames) {
-      let { name, type, location } = module
+      let { name, type, location } = module;
       cy.readFile(location, {log:false})
         .then(source => Cypress.modules.push({ name, type, location, source }))
     }
   })
-})
+});
